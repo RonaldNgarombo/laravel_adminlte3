e27b9b8ebdf24a90489b1c97fc13173b318b92b2
Ronald Ngarombo <ronaldngzgadro@gmail.com>
2023-11-29 00:44:59 +0300

    Create guest.blade.php

---
+++ resources/views/components/layouts/guest.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Page Title' }} | {{ env('APP_NAME') }}</title>
+
+    {{-- Theme style --}}
+    <link rel="stylesheet" href="{{ asset('theme/dist/css/adminlte.min.css') }}">
+</head>
+
+<body class="font-sans antialiased">
+    {{ $slot }}
+</body>
+
+</html>
