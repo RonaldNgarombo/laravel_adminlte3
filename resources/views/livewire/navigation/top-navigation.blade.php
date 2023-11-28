203827fe93727fd40f64db6cf8e5e0d9d8133d19
Ronald Ngarombo <ronaldngzgadro@gmail.com>
2023-11-29 00:45:12 +0300

    Update top-navigation.blade.php

--- resources/views/livewire/navigation/top-navigation.blade.php
+++ resources/views/livewire/navigation/top-navigation.blade.php
@@ -152,6 +152,23 @@
             </div>
         </li>
 
+        <!-- Notifications Dropdown Menu -->
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="far fa-user"></i>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
+                <a href="#" class="dropdown-item">
+                    <i class="fas fa-user mr-2"></i> Profile
+                </a>
+
+                <a wire:click="logoutUser" class="dropdown-item" style="cursor: pointer;">
+                    <i class="fas fa-sign-out-alt mr-2"></i> Logout
+                </a>
+            </div>
+        </li>
+
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
