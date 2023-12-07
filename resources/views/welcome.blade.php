e82e231c805be25e47949b2a577208b55f54543a
Ronald Ngarombo <ronaldngzgadro@gmail.com>
2023-12-07 15:31:47 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -884,8 +884,6 @@
                             </p>
                         </div>
 
-                        <span>Ngz - Safety Hive</span>
-
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
